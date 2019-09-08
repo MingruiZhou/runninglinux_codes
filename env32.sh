52115f80cb2607156a029212d667b293021d5592
MingruiZhou <MingruiZhou@outlook.com>
2019-09-07 23:01:01 -0700

    add arm32 setenv file

---
+++ env32.sh
@@ -0,0 +1,11 @@
+export KERNEL_DIR=/home/rlk/rlk_basic/runninglinuxkernel_4.0
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+export AS=${CROSS_COMPILE}as
+export LD=${CROSS_COMPILE}ld
+export CC=${CROSS_COMPILE}gcc
+export AR=${CROSS_COMPILE}ar
+export NM=${CROSS_COMPILE}nm
+export STRIP=${CROSS_COMPILE}strip
+export OBJCOPY=${CROSS_COMPILE}objcopy
+export OBJDUMP=${CROSS_COMPILE}objdump
